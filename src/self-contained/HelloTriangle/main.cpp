496a33d3fa0034f0e81530c8f14aa22f81827a87
Wasin Thonkaew <haxpor@gmail.com>
2019-10-24 16:41:08 +0800

    Fix to make HellTriangle (self-contained) works

--- src/self-contained/HelloTriangle/main.cpp
+++ src/self-contained/HelloTriangle/main.cpp
@@ -126,6 +126,7 @@ void main()
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glEnableVertexAttribArray(0);
     glBindVertexArray(0);
 }
 
