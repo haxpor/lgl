1740ba8798e0e47ad739b69c9d7c760c8c585f41
Wasin Thonkaew <haxpor@gmail.com>
2019-10-25 15:20:12 +0800

    Include glfw3 in more self-contained header lgl.h

--- includes/lgl/lgl.h
+++ includes/lgl/lgl.h
@@ -5,5 +5,10 @@
 #include "lgl/Util.h"
 #include "lgl/Error.h"
 #include "lgl/Shader.h"
+#include <GLFW/glfw3.h>
+
+// include the most frequently used at this level
+#define LGL_EXTERNAL_GLM_INCLUDE
+#include "lgl/External.h"
 
 #endif /* LGL_H_ */
