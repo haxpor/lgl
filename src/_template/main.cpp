1e4a97c4ef130e2b6968e919d8f5d581c5793ea9
Wasin Thonkaew <haxpor@gmail.com>
2019-11-04 16:16:59 +0800

    Add template main.cpp file for building up other testbed quickly

---
+++ src/_template/main.cpp
@@ -0,0 +1,275 @@
+/**
+ * Template main.cpp file for building up other testbed quickly.
+ *
+ * This template will render opaque white color rotating cube.
+ * Camera is at 0.0, 0.0, 3.0 and points at origin with +y-axis is up vector.
+ *
+ * The part that you will likely need to remove is cube's vertices, and indices definition.
+ */
+#include "lgl/lgl.h"
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+int screenWidth = 800;
+int screenHeight = 600;
+GLFWwindow* window = nullptr;
+double prevTicks;
+double lastMouseX, lastMouseY;
+
+////////////////////////
+/// system callbacks
+////////////////////////
+void sys_mouseCB(GLFWwindow* window, double x, double y);
+
+////////////////////////
+/// glfw callback
+////////////////////////
+int initGLFW(int argc, char** argv);
+void initGL();
+
+void initMem();
+void destroyMem();
+void reshapeCB(GLFWwindow* window, int w, int h);
+void keyboardCB(double deltaTime);
+void mouseCB(GLFWwindow* window, double dx, double dy);
+void mouseScrollCB(GLFWwindow* window, double dx, double dy);
+void update(double dt);
+void render();
+
+////////////////////////
+/// global variables
+////////////////////////
+GLuint vao;
+GLuint vbo;
+lgl::Shader shader;
+
+float vertices[] = {
+    // positions          // texture coords
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+unsigned int indices[] =
+{
+    0, 1, 2,
+    0, 2, 3
+};
+
+////////////////////////
+// implementations
+////////////////////////
+int initGLFW(int argc, char** argv)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(screenWidth, screenHeight, argv[0], nullptr, nullptr);
+    if (window == nullptr)
+    {
+        lgl::error::ErrorWarn("Failed to create GLFW window");
+        std::exit(1);
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, reshapeCB);
+    glfwSetCursorPosCallback(window, sys_mouseCB);
+    glfwSetScrollCallback(window, mouseScrollCB);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        lgl::error::ErrorWarn("Failed to initalize GLAD");
+        std::exit(1);
+        return -1;
+    }
+
+    glViewport(0, 0, screenWidth, screenHeight);
+
+    return 0;
+}
+
+void initGL()
+{
+    GLint majorVersion, minorVersion;
+    glGetIntegerv(GL_MAJOR_VERSION, &majorVersion);
+    glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
+    std::cout << "OpenGL version in use: " << majorVersion << "." << minorVersion << std::endl;
+
+    glEnable(GL_DEPTH_TEST);
+    
+    // create shader
+    int result = shader.Build("data2/trans.vert", "data2/color.frag");
+    LGL_ERROR_QUIT(result, "Error creating shader");
+
+    // prepare vao
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+        glGenBuffers(1, &vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), nullptr);
+        glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    // set uniform values
+    shader.Use();
+
+    // compute projection * view matrix
+    glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 pv = glm::perspective(glm::radians(60.0f), screenWidth * 1.0f / screenHeight, 0.1f, 100.0f);
+    pv = pv * view;
+    glUniformMatrix4fv(shader.GetUniformLocation("pv"), 1, GL_FALSE, glm::value_ptr(pv));
+
+    glUniformMatrix4fv(shader.GetUniformLocation("model"), 1, GL_FALSE, glm::value_ptr(glm::mat4(1.0f)));
+    glUniform3f(shader.GetUniformLocation("color"), 1.0f, 1.0f, 1.0f);
+
+    std::cout << glfwGetVersionString() << std::endl;;
+}
+
+void update(double dt)
+{
+
+}
+
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    shader.Use();
+
+    glBindVertexArray(vao);
+        const float angle = 20.0f;
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, static_cast<float>(glfwGetTime()) * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+        glUniformMatrix4fv(shader.GetUniformLocation("model"), 1, GL_FALSE, glm::value_ptr(model));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
+}
+
+void reshapeCB(GLFWwindow* window, int w, int h)
+{
+    screenWidth = w;
+    screenHeight = h;
+    
+    glViewport(0, 0, static_cast<GLsizei>(screenWidth), static_cast<GLsizei>(screenHeight));
+}
+
+void sys_mouseCB(GLFWwindow* window, double x, double y)
+{
+    static bool firstMouse = true;
+    if (firstMouse)
+    {
+        lastMouseX = x;
+        lastMouseY = y;
+        firstMouse = false;
+    }
+
+    float xoffset = x - lastMouseX;
+    float yoffset = lastMouseY - y;
+    lastMouseX = x;
+    lastMouseY = y;
+
+    static float kSensitivity = 0.05f;
+    xoffset *= kSensitivity;
+    yoffset *= kSensitivity;
+
+    mouseCB(window, xoffset, yoffset);
+}
+
+void mouseCB(GLFWwindow* window, double x, double y)
+{
+}
+
+void mouseScrollCB(GLFWwindow* window, double dx, double dy)
+{
+}
+
+void keyboardCB(double deltaTime)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+        return;
+    }
+}
+
+void initMem()
+{
+}
+
+void destroyMem()
+{
+    glDeleteBuffers(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    shader.Destroy();
+}
+
+int main(int argc, char** argv)
+{
+    initMem();
+
+    initGLFW(argc, argv);
+    initGL();
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        double delta = glfwGetTime() - prevTicks;
+        prevTicks = glfwGetTime();
+
+        keyboardCB(delta);
+        update(delta);
+        render();
+
+        glfwSwapBuffers(window);
+    }
+
+    destroyMem();
+    glfwTerminate();
+
+    return 0;
+}
