8a1d20e8dc974934aa0ab93d711ecbcb4ee21ae2
Wasin Thonkaew <haxpor@gmail.com>
2019-11-07 13:49:19 +0800

    Add comment about how glm::to_string works

--- src/ModelViewMatrix/main.cpp
+++ src/ModelViewMatrix/main.cpp
@@ -1,5 +1,18 @@
 /**
  * Inspect the result of model * view matrix.
+ *
+ * Note:
+ *  - glm::to_string() will print each column-elements one by one thus
+ *
+ *  let say the following is the matrix element layout
+ *  [1 5 9  13]
+ *  [2 6 10 14]
+ *  [3 7 11 15]
+ *  [4 8 12 16]
+ *
+ *  so for glm::mat2 it will print
+ *
+ *  mat2x2((1,2,3,4), (5,6,7,8), (9,10,11,12), (13,14,15,16))
  */
 
 #include <iostream>
