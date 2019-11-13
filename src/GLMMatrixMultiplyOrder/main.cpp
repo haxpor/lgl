a0f780a6fab73cd9f94faf756fc76e1cfb6dd53f
Wasin Thonkaew <haxpor@gmail.com>
2019-11-14 01:57:58 +0800

    Refactor inclusion of header, add proper Makefile, and add more testcases in GLMMatrixMultiplyOrder

--- src/GLMMatrixMultiplyOrder/main.cpp
+++ src/GLMMatrixMultiplyOrder/main.cpp
@@ -2,12 +2,16 @@
  * Prove of concept that GLM is pre-multiplication matrix order.
  *
  * GLM matrix multiplication is pre-multiplication, thus the order of transformation call can be
- * thought as beginning from left to right e.g. translate rotate | deep down matrix multiplication
- * will be rotate * translate thus rotate happens first before translation.
+ * thought as beginning from left to right e.g.
+ *  m = translate operation
+ *  m = rotate operation
+ *
+ * deep down GLM will do translate_matrix * rotation_matrix. So the last operation will be executed first.
  */
 
 #include <iostream>
-#include "lgl/lgl.h"
+#include "glm/glm.hpp"
+#include "glm/gtx/string_cast.hpp"
 
 int main()
 {
@@ -25,17 +29,37 @@ int main()
         glm::mat4 m = glm::mat4(1.0f);
         m = glm::translate(m, glm::vec3(2.0f,  3.0f,  4.0f)); 
         m = glm::rotate(m, glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        std::cout << glm::to_string(m) << std::endl;
+
+        assert(m[3][0] == 2.0f &&
+               m[3][1] == 3.0f &&
+               m[3][2] == 4.0f && "Translation components must not change");
     }
 
     // translate then rotate
     // this produces orbitting effect, thus affect the position of the object
-    // notice translation elements at 4, 8, 12 which will be effected by rotation.
+    // notice translation elements at 4, 8, 12 which will be affected by rotation.
     {
         glm::mat4 m = glm::mat4(1.0f);
         m = glm::rotate(m, glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
         m = glm::translate(m, glm::vec3(2.0f,  3.0f,  4.0f)); 
-        std::cout << glm::to_string(m) << std::endl;
+
+        assert(m[3][1] != 3.0f &&
+               m[3][2] != 4.0f && "Translation components must changed except x component");
+    }
+
+    // another example
+    {
+        // build the matrix from sequence of operations to be based as comparison
+        glm::mat4 m1 = glm::mat4(1.0f);
+        m1 = glm::translate(m1, glm::vec3(2.0f,  3.0f,  4.0f)); 
+        m1 = glm::rotate(m1, glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        m1 = glm::scale(m1, glm::vec3(1.0f, 1.0f, 1.0f));
+
+        // equally to this, each individual tranformation matrix will operate based on top of identity matrix
+        glm::mat4 iden = glm::mat4(1.0f);
+        glm::mat4 m2 = glm::translate(iden, glm::vec3(2.0f,  3.0f,  4.0f)) * glm::rotate(iden, glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f)) * glm::scale(iden, glm::vec3(1.0f, 1.0f, 1.0f));
+
+        assert(m1[0][0] == m2[0][0] && "Two matrices must be equal");
     }
 
     return 0;
